c3afe6d30598b5c5616fe1feffb4af1e616527c0
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-10-27 13:25:15 +0200

    Challenge POO4 terminé

--- Car.php
+++ Car.php
@@ -2,4 +2,29 @@
 
 class Car extends Vehicle
 {
+    ////////// *** CHALLENGE POO4 *** //////////
+    private bool $hasParkBrake = false;
+
+    /**
+     * Get the value of hasParkBrake
+     */
+    public function getHasParkBrake(): bool
+    {
+        return $this->hasParkBrake;
+    }
+
+    public function setHasParkBrake(bool $hasParkBrake): self
+    {
+        $this->hasParkBrake = $hasParkBrake;
+        return $this;
+    }
+
+    public function start(): string
+    {
+        if ($this->getHasParkBrake()) {
+            throw new Exception('WARNING ! Park brake is activated !');
+        }
+        echo $this->forward(15);
+        return 'Go ! Drive safe.';
+    }
 }
--- index.php
+++ index.php
@@ -9,59 +9,80 @@ require 'Skateboard.php';
 require 'Car.php';
 
 //// Tableau currentVehicles vide dans MotorWay ////
-$motorWay1 = new MotorWay();
+// $motorWay1 = new MotorWay();
 
-var_dump($motorWay1);
+// var_dump($motorWay1);
 
 
 //// Doit s'ajouter au tableau currentVehicles de MotorWay ////
-$audi = new Car('red', 4);
-$micra = new Car('green', 5);
+// $audi = new Car('red', 4);
+// $micra = new Car('green', 5);
 
-$motorWay1->addVehicle($audi);
-$motorWay1->addVehicle($micra);
+// $motorWay1->addVehicle($audi);
+// $motorWay1->addVehicle($micra);
 
-var_dump($motorWay1);
+// var_dump($motorWay1);
 
 
 //// Ne doit pas s'ajouter au tableau currentVehicles de MotorWay////
-$bike1 = new Bike('green', 1);
-$skate1 = new Skateboard('blue', 1);
+// $bike1 = new Bike('green', 1);
+// $skate1 = new Skateboard('blue', 1);
 
 
-$motorWay1->addVehicle($bike1);
-$motorWay1->addVehicle($skate1);
+// $motorWay1->addVehicle($bike1);
+// $motorWay1->addVehicle($skate1);
 
-var_dump($motorWay1);
+// var_dump($motorWay1);
 
 
 
 //// TEST avec PedestrianWay ////
-$pedestrianWay1 = new PedestrianWay();
+// $pedestrianWay1 = new PedestrianWay();
 
-var_dump($pedestrianWay1);
+// var_dump($pedestrianWay1);
 
 
-$bmw = new Car('red', 4);
-$pedestrianWay1->addVehicle($bmw);
+// $bmw = new Car('red', 4);
+// $pedestrianWay1->addVehicle($bmw);
 
-$skate2 = new Skateboard('yellow', 1);
-$pedestrianWay1->addVehicle($skate2);
+// $skate2 = new Skateboard('yellow', 1);
+// $pedestrianWay1->addVehicle($skate2);
 
-var_dump($pedestrianWay1);
+// var_dump($pedestrianWay1);
 
 
 
 //// TEST avec ResidentialWay ////
-$ResidentialWay1 = new ResidentialWay();
+// $ResidentialWay1 = new ResidentialWay();
 
-var_dump($ResidentialWay1);
+// var_dump($ResidentialWay1);
 
 
-$ferrari = new Car('red', 2);
-$ResidentialWay1->addVehicle($ferrari);
+// $ferrari = new Car('red', 2);
+// $ResidentialWay1->addVehicle($ferrari);
 
-$bike2 = new Bike('violet', 1);
-$ResidentialWay1->addVehicle($bike2);
+// $bike2 = new Bike('violet', 1);
+// $ResidentialWay1->addVehicle($bike2);
 
-var_dump($ResidentialWay1);
+// var_dump($ResidentialWay1);
+
+
+////////// TEST CHALLENGE POO4 //////////
+$r8 = new Car('black', 2);
+
+echo '<h4>Test capture erreur sans le frein à main et sans capture d\'erreur :</h4>';
+var_dump($r8);
+echo $r8->start();
+
+echo '<h4>Test capture erreur avec le frein à main :</h4>';
+$r8->setHasParkBrake(true);
+var_dump($r8);
+try {
+    echo $r8->start() . PHP_EOL;
+} catch (Exception $e) {
+    echo 'Exception received  : ' . $e->getMessage() . PHP_EOL;
+    $r8->setHasParkBrake(false);
+    var_dump($r8);
+} finally {
+    echo "Ma voiture roule comme un donut !" . PHP_EOL;
+}
